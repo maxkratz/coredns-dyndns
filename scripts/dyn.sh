1fcd80a8db9f2e31fddf2acfd4cb19cd1e954ee7
Maximilian Kratz <account@maxkratz.com>
2021-04-13 17:42:53 +0200

    Bugfix for serial: Increment old serial if new timestamp is not larger

--- scripts/dyn.sh
+++ scripts/dyn.sh
@@ -29,7 +29,14 @@ sed "s/@ IN A .*/@ IN A $IP/" /zonefile | tee /zonefile.tmp >/dev/null
 
 # Get old serial from zonefile and set value to current date/time stamp
 SERIAL_OLD=$(cat /zonefile.tmp | grep "; serial" | tr -dc '0-9')
-SERIAL_NEW=$(date +%Y%m%d%M%S)
+SERIAL_NEW=$(date +%Y%m%d%M)
+
+# Ensure that new serial is always larger than previous one
+# (This could happen if there is more than one update per minute)
+if [ "$SERIAL_OLD" -ge "$SERIAL_NEW" ]; then
+    echo "=> Incremented old serial."
+    SERIAL_NEW=$((SERIAL_OLD+1))
+fi
 
 # Replace old serial with new value in temp zonefile
 sed -i "s/$SERIAL_OLD/$SERIAL_NEW/" /zonefile.tmp
