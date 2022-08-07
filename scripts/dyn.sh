0d1c34b0664fc0d33f23b69547ba003b8a1755a2
Maximilian Kratz <account@maxkratz.com>
2022-08-07 17:21:55 +0200

    Quick workaround for DynDNSv2 return codes

--- scripts/dyn.sh
+++ scripts/dyn.sh
@@ -9,24 +9,27 @@ HOSTNAME=$2
 # Check if provided IP parameter was empty
 if [ -z "$IP" ]
 then
-    echo "=> No IP provided. Aborting." >&2 
+    # echo "=> No IP provided. Aborting." >&2
+    echo "badsys"
     exit 1;
 fi
 
-if [ ! -z "$HOSTNAME" ]
-then
-    echo "=> Hostname parameter: $HOSTNAME"
-fi
+# if [ ! -z "$HOSTNAME" ]
+# then
+    # echo "=> Hostname parameter: $HOSTNAME"
+# fi
 
 # Check if IP is valid
 IP_REGEX="^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 VALID=$(echo $IP | grep -oE "$IP_REGEX" || true)
 if [ -z "$VALID" ]
 then
-    echo "=> Provided IP is not a valid IPv4 address. Aborting." >&2 
+    # echo "=> Provided IP is not a valid IPv4 address. Aborting." >&2
+    echo "badsys"
     exit 2;
 else
-    echo "=> Update IP to $IP."
+    # echo "=> Update IP to $IP."
+    echo "good"
 fi
 
 # Replace old IP from zonefile by new IP, use tee instead of sed -i
@@ -40,7 +43,7 @@ SERIAL_NEW=$(date +%y%m%d%H%M)
 # Ensure that new serial is always larger than previous one
 # (This could happen if there is more than one update per minute)
 if [ "$SERIAL_OLD" -ge "$SERIAL_NEW" ]; then
-    echo "=> Incremented old serial."
+#    echo "=> Incremented old serial."
     SERIAL_NEW=$((SERIAL_OLD+1))
 fi
 
